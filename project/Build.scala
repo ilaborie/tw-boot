6be850701a2bd61f2d77e8a98477c2e7419dd153
Igor Laborie <i.laborie@akka.eu>
2012-06-29 01:08:24 +0200

    Use Bootstrap with Less

--- project/Build.scala
+++ project/Build.scala
@@ -4,15 +4,21 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "tw-boot"
-    val appVersion      = "1.0-SNAPSHOT"
+	val appName         = "tw-boot"
+	val appVersion      = "1.0-SNAPSHOT"
 
-    val appDependencies = Seq(
+	val appDependencies = Seq(
       // Add your project dependencies here,
-    )
+      )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      // Add your own project settings here      
-    )
+	val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
+		lessEntryPoints <<= baseDirectory(customLessEntryPoints)
+		)
 
+    // Only compile the bootstrap bootstrap.less file and any other *.less file in the stylesheets directory
+    def customLessEntryPoints(base: File): PathFinder = (
+    	(base / "app" / "assets" / "stylesheets" / "bootstrap" / "bootstrap.less") +++
+    	(base / "app" / "assets" / "stylesheets" / "bootstrap" / "bootstrap-responsive.less") +++
+    	(base / "app" / "assets" / "stylesheets" * "*.less")
+    	)
 }
